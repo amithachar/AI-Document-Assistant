d8dc467dea14d93a296c04f081b6e2a3ec636346
Your Name <your_email@example.com>
2026-04-05 13:23:24 +0530

    ddsdewdewde

--- rag-assistant/app/embedder.py
+++ rag-assistant/app/embedder.py
@@ -1,7 +1,14 @@
-from sentence_transformers import SentenceTransformer
+import requests
+import os
 
-# Fast + lightweight model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+OLLAMA_URL = os.getenv("OLLAMA_URL", "http://ollama-service:11434/api/embeddings")
 
 def get_embeddings(texts):
-    return model.encode(texts).tolist()
+    embeddings = []
+    for text in texts:
+        res = requests.post(
+            OLLAMA_URL,
+            json={"model": "mistral", "prompt": text}
+        )
+        embeddings.append(res.json()["embedding"])
+    return embeddings
